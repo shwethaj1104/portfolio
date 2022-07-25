52e840815dab53f3e638a5a5718fc1826c40d6f4
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-25 15:53:09 +0530

    changes for newsletter

--- personal-portfolio/src/components/Contact.jsx
+++ personal-portfolio/src/components/Contact.jsx
@@ -54,19 +54,19 @@ export const Contact = () => {
                     <h2>Get In Touch</h2>
                     <form onSubmit={handleSubmit}>
                         <Row>
-                            <Col sm={6} classNamepx-1>
+                            <Col sm={6} className="px-1">
                                 <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e)=> onFormUpdate('firstName',e.target.value)}/>
                             </Col>
-                            <Col sm={6} classNamepx-1>
+                            <Col sm={6} className="px-1">
                                 <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=> onFormUpdate('lastName',e.target.value)}/>
                             </Col>
-                            <Col sm={6} classNamepx-1>
+                            <Col sm={6} className="px-1">
                                 <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e)=> onFormUpdate('email',e.target.value)}/>
                             </Col>
-                            <Col sm={6} classNamepx-1>
+                            <Col sm={6} className="px-1">
                                 <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e)=> onFormUpdate('phone',e.target.value)}/>
                             </Col>
-                            <Col sm={6} classNamepx-1>
+                            <Col sm={6} className="px-1">
                                 <textarea value={formDetails.message} placeholder="Message" onChange={(e)=> onFormUpdate('message',e.target.value)}/>
                                 <button type="submit"><span>{buttonText}</span></button>
                             </Col>
