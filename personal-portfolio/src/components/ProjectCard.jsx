2506735cef74449fd5345179c38ad3717a1c7e29
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-08 20:44:32 +0530

    changes for viewing each project demo and code

--- personal-portfolio/src/components/ProjectCard.jsx
+++ personal-portfolio/src/components/ProjectCard.jsx
@@ -2,17 +2,25 @@ import { Col } from "react-bootstrap"
 import projImg1 from '../assets/img/project-img1.png'
 
 
-export const ProjectCard = ({title,description,imgUrl}) => {
+export const ProjectCard = ({ title, description, imgUrl,gitUrl,demoUrl }) => {
   // let requiredImgUrl =  imgUrl.replace(/['"]+/g, '');
   return (
-   <Col size={12} sm={6} md={4}>
-        <div className="proj-imgbx">
-            <img className="proj-imgbx__image" src={`/assets/img/${imgUrl}`}/>
-            <div className="proj-txtx">
-                <h4>{title}</h4>
-                <span>{description}</span>
-            </div>
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img className="proj-imgbx__image" src={`/assets/img/${imgUrl}`} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
         </div>
-   </Col>
+      </div>
+      <div className="project-btn">
+        <a className='git' href={gitUrl} target={'_blank'}>
+        <button className="code">Git</button>
+        </a>
+        <a className='git' href={demoUrl} target={'_blank'}>
+          <button className="demo">Live Demo</button>
+        </a>
+      </div>
+    </Col>
   )
 }
