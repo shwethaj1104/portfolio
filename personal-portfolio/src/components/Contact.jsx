3cb5582ca4e4de980a8df4fa03c5043ed0e14b69
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-18 13:27:26 +0530

    Added Gmail contact ability

--- personal-portfolio/src/components/Contact.jsx
+++ personal-portfolio/src/components/Contact.jsx
@@ -1,8 +1,24 @@
 import { useState } from "react"
 import { Col, Container, Row } from "react-bootstrap";
-import contactImg from '../assets/img/contact-img.svg'
+import contactImg from '../assets/img/contact-img.svg';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_a3gfvee', 'template_njg1san', form.current, 'HmzkIsNtHj0JIc_53')
+        .then((result) => {
+            console.log(result.text);
+            e.target.reset()
+            setFormDetails(formInitialDetails)
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     const formInitialDetails = {
         firstName: '',
         lastName: '',
@@ -52,22 +68,22 @@ export const Contact = () => {
                 </Col>
                 <Col md={6}>
                     <h2>Get In Touch</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form ref={form} onSubmit={sendEmail}>
                         <Row className="contact-row">
                             <Col sm={6} className="px-1">
-                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e)=> onFormUpdate('firstName',e.target.value)}/>
+                                <input type="text" name="name" value={formDetails.firstName} placeholder="Name" onChange={(e)=> onFormUpdate('firstName',e.target.value)}/>
+                            </Col>
+                            {/* <Col sm={6} className="px-1">
+                                <input type="text" name="last_name" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=> onFormUpdate('lastName',e.target.value)}/>
+                            </Col> */}
+                            <Col sm={6} className="px-1">
+                                <input type="email" name="email" value={formDetails.email} placeholder="Email Address" onChange={(e)=> onFormUpdate('email',e.target.value)}/>
                             </Col>
                             <Col sm={6} className="px-1">
-                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=> onFormUpdate('lastName',e.target.value)}/>
+                                <input type="tel" name="phone" value={formDetails.phone} placeholder="Phone Number" onChange={(e)=> onFormUpdate('phone',e.target.value)}/>
                             </Col>
                             <Col sm={6} className="px-1">
-                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e)=> onFormUpdate('email',e.target.value)}/>
-                            </Col>
-                            <Col sm={6} className="px-1">
-                                <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e)=> onFormUpdate('phone',e.target.value)}/>
-                            </Col>
-                            <Col sm={6} className="px-1">
-                                <textarea value={formDetails.message} placeholder="Message" onChange={(e)=> onFormUpdate('message',e.target.value)}/>
+                                <textarea name="message" value={formDetails.message} placeholder="Message" onChange={(e)=> onFormUpdate('message',e.target.value)}/>
                                 <button className="px-1__button" type="submit"><span>{buttonText}</span></button>
                             </Col>
                             {
