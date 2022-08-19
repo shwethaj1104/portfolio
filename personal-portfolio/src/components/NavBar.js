7ce63c51ba4b360b1165a0089f481cfb1f2c7fa6
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-19 16:26:55 +0530

    Formatting

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -2,25 +2,25 @@ import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {BsLinkedin,BsGithub} from 'react-icons/bs'
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
 
 
 
 export const NavBar = () => {
-    const [activeLink,setActiveLink] = useState('home')
-    const[scrolled,setScrolled] = useState(false);
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false);
 
-    useEffect(()=>{
-        const onScroll = () =>{
-            if(window.scrollY > 50){
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
                 setScrolled(true)
-            }else{
+            } else {
                 setScrolled(false)
             }
         }
-        window.addEventListener('scroll',onScroll)
-        return ()=> window.removeEventListener('scroll',onScroll)
-    },[])
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value)
@@ -34,19 +34,19 @@ export const NavBar = () => {
                     {/* <img src="/assets/img/my-self.png" alt='' /> */}
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
-                   <span className='navbar-toggler-icon'></span>
+                    <span className='navbar-toggler-icon'></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                        <Nav.Link href="#connect" className={activeLink === 'connect' ? 'active navbar-link vvd' : 'navbar-link vvd'} onClick={()=>onUpdateActiveLink('connect')}>Let's Connect</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                        <Nav.Link href="#connect" className={activeLink === 'connect' ? 'active navbar-link vvd' : 'navbar-link vvd'} onClick={() => onUpdateActiveLink('connect')}>Let's Connect</Nav.Link>
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='https://github.com/shwethaj1104/' target={'_blank'}><BsGithub className='svg_icons'/></a>
-                            <a href='https://www.linkedin.com/in/shwetha-j-b26883159/' target={'_blank'}><BsLinkedin className='svg_icons'/></a>
+                            <a href='https://github.com/shwethaj1104/' target={'_blank'}><BsGithub className='svg_icons' /></a>
+                            <a href='https://www.linkedin.com/in/shwetha-j-b26883159/' target={'_blank'}><BsLinkedin className='svg_icons' /></a>
                         </div>
                     </span>
                 </Navbar.Collapse>
