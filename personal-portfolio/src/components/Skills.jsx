4ff34043a97f68fcd7d9e7e111242cf83e7fd367
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-23 12:12:30 +0530

    Formatting

--- personal-portfolio/src/components/Skills.jsx
+++ personal-portfolio/src/components/Skills.jsx
@@ -1,5 +1,5 @@
 
-import { Col, Container,Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import meter1 from '../assets/img/meter1.svg';
@@ -11,43 +11,43 @@ import DATA from '../Data.json'
 
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
   return (
     <section className="skill" id="skills">
       <Container>
         <Row>
           <Col>
-          <div className="skill-bx">
-            <h2>Skills</h2>
-            <p>Front end Developer</p>
-            <Carousel className="skill-slider" responsive={responsive} infinite={true}>
-            {DATA.projects.skills.map((project, index) => {
-                      return (
-                        <div key={index} className="item">
-                        <img  src={`/assets/img/${project.imgUrl}`} alt="Image" />
-                        <h5>{project.title}</h5>
-                      </div>
-                      )
-                    })}
-            </Carousel>
-          </div></Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>Front end Developer</p>
+              <Carousel className="skill-slider" responsive={responsive} infinite={true}>
+                {DATA.projects.skills.map((project, index) => {
+                  return (
+                    <div key={index} className="item">
+                      <img src={`/assets/img/${project.imgUrl}`} alt="Image" />
+                      <h5>{project.title}</h5>
+                    </div>
+                  )
+                })}
+              </Carousel>
+            </div></Col>
         </Row>
       </Container>
       <img src={colorSharp} alt="colorSharp" className="background-image-left" />
