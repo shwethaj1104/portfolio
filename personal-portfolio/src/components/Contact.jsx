19e1dfe9aed235c526da61c2b9e6ee66f8b75df2
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-28 11:11:39 +0530

    Formatting

--- personal-portfolio/src/components/Contact.jsx
+++ personal-portfolio/src/components/Contact.jsx
@@ -55,21 +55,6 @@ export const Contact = () => {
     //     e.preventDefault();
     //     setButtonText('Sending...')
     //     let response = await fetch("http://localhost:5000/contact",{
-    //         method:"POST",
-    //         headers:{
-    //             "Content-Type":"Application/json;charset=utf-8",
-    //         },
-    //         body: JSON.stringify(formDetails),
-    //     });
-    //     setButtonText("Send");
-    //     let result = response.json();
-    //     setFormDetails(formInitialDetails);
-    //     if(result.code === 200){
-    //         setStatus({success:true,message:'Message Sent Successfully'})
-    //     }else{
-    //         setStatus({success:false,message:"Something went wrong, please try again later"})
-    //     }
-    // }
 
 
 
